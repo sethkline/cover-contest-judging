2c6e79c7b7b44566de0809028e8dbfc63e34ecf0
Seth Kline <seth.mkline@gmail.com>
2025-02-26 00:43:20 -0500

    update import

--- src/components/judge/dashboard/ContestCard.tsx
+++ src/components/judge/dashboard/ContestCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/Card";
 import CategoryCard from "./CategoryCard";
 
 interface Category {
